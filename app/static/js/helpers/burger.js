22c46b9fe004e2e2b5cde38fe02c1fe4e0ee335d
ajlee4 <patsyno94@gmail.com>
2020-02-16 19:02:58 +0300

    add mobile-menu

--- app/static/js/helpers/burger.js
+++ app/static/js/helpers/burger.js
@@ -4,9 +4,12 @@
 $(document).ready(function(){
     $('#header-mobile-menu').on('click', function (e) {
         e.preventDefault();
+        $("a[data-wrap='sublist-wrap']").removeClass('open').siblings().slideUp()
+        $('#mobile-body-tel-list').fadeOut()
+          
         $(this).toggleClass('burger-close');
-        // $('.header__mobile-menu').fadeToggle();
-        // $('body').toggleClass('header-menu-opened');
+        $('.header__mobile-bottom').fadeToggle();
+        $('body').toggleClass('header-menu-opened');
        
       });
 })
--- app/static/js/helpers/button.js
+++ app/static/js/helpers/button.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
-    $('#search-button').on('click',(e)=> {
-        e.preventDefault()
-        $('#search-input').slideToggle()
-        $('#search-button .close-icon').toggle()
-        $('#search-button .search-icon').toggle()
-    })
+
+    if (document.body.clientWidth > 1024) {
+        $('#search-button').on('click',(e)=> {
+            console.log($(this))
+            e.preventDefault()
+            $('#search-input').slideToggle()
+            $('#search-button .close-icon').toggle()
+            $('#search-button .search-icon').toggle()
+        })
+    }
+
 
 
     $("a[data-close='close-popup']").on('click',(e)=> {
@@ -12,8 +17,18 @@ $(document).ready(function(){
         Popups.hide()
     })
 
+    $("a[data-wrap='sublist-wrap']").on('click',(e)=> {
+        const target = $( event.target );
+        e.preventDefault()
+        
+        target.toggleClass('open')
+        target.siblings().slideToggle()
+    })
+
     $('#mobile-tel').on('click',(e)=> {
         e.preventDefault()
+        $('.header__mobile-bottom').fadeOut();
+        $('#header-mobile-menu').removeClass('burger-close');
         $('#mobile-body-tel-list').fadeIn(300)   
         
     })
